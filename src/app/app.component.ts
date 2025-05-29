200da5269d200919c27839a404dc1a88c8a457f0
Anner Escobar <anner123escobar@gmail.com>
2025-05-28 21:50:42 -0600

    cambios actualizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   imports: [
-    DashboardComponent,
     MatToolbarModule,
     MatCardModule,
     MatListModule,
