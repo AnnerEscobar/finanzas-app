89e839d9db581ce10894990e5ecfcefc32f498a1
Anner Escobar <anner123escobar@gmail.com>
2025-06-12 21:45:20 -0600

    modificando url

--- src/environment.prod.ts
+++ src/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'finanzas-app-backend-production-0665.up.railway.app'
+  apiUrl: 'https://finanzas-app-backend-production-0665.up.railway.app'
    /* apiUrl: 'http://localhost:3000' */
    /*apiUrl: 'https://7k1k7txz-3000.use2.devtunnels.ms' */
 };
